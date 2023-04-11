62427699e1f76bb4068582ef167da4cd9b686fcb
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2023-04-11 22:32:02 +0600

    Cleaned up the app file

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Helmet } from 'react-helmet';
+
+
+function App() {
+    return (
+        <div className="app">
+            <Helmet>
+                <title>Instagram Downloader</title>
+            </Helmet>
+            
+        </div>
+    );
+}
+
+export default App;
